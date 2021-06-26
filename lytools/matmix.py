2346145e935b16d5ae7c0de67a68753f30547165
Liebranca <dantepaola25@gmail.com>
2021-06-26 14:09:51 -0300

    Matmix baking optimizations
    Greatly reduced baking times for matmix by having all four potential materials bake each map in one go. Solved some issues with bake lines on the resulting unified mix.

--- lytools/matmix.py
+++ lytools/matmix.py
@@ -1,4 +1,4 @@
-import bpy, os;
+import bpy, os; import numpy as np;
 
 from bpy.types import Panel, Operator, PropertyGroup, Object, Material, Scene;
 from bpy.utils import register_class, unregister_class;
@@ -73,6 +73,9 @@ def BKMATIDS(me):
     for vckey in ["Col", "Mix"]:
         ob.data.vertex_colors.new(name=vckey);
 
+    ob.lytools.par=cl;
+    ob.name=f"{cl.name}_MIX"; ob.data.name=ob.name;
+
 #   ---     ---     ---     ---     ---
 # paste submat ids into 'COL' slot
 
@@ -125,7 +128,7 @@ def BKMATIDS(me):
     for x in range(h):
         cmat=bpy.data.materials["COLORMASKING"].copy();
 
-        cmat.name=(ob.name)+f'_COLORMASK{x}'; i=0; j=0;
+        cmat.name=(ob.lytools.par.name)+f'_COLORMASK{x}'; i=0; j=0;
         ob.data.materials[x]=cmat; nodes=cmat.node_tree.nodes;
 
         for mat in me.materials:
@@ -137,12 +140,13 @@ def BKMATIDS(me):
     for i in range(len(ob.data.materials)-1, -1, -1):
         ob.lytools.mix_idex=i;
 
+    cl.select=0;
+
 #   ---     ---     ---     ---     ---
 
 def STALPHA(col, alpha):
-    c_pixels=list(col.pixels); a_pixels=list(alpha.pixels);
-    for j in range(0, len(c_pixels), 4): c_pixels[j+3]*=a_pixels[j];
-    col.pixels[:]=c_pixels; col.update();
+    a=np.array(col.pixels); b=np.array((alpha.pixels[:])[0::4]);
+    a[3::4]=np.multiply(a[3::4], b); col.pixels=a;
 
 def BKMIXMAT():
 
@@ -155,120 +159,98 @@ def BKMIXMAT():
     rend.image_settings.file_format='PNG';
     rend.image_settings.color_mode='RGBA';
 
-    folder=f"{ob.lytools.f0}\\textures\\{ob.name}"
+    folder=f"{ob.lytools.f0}\\textures\\{ob.lytools.par.name}"
     if not os.path.exists(folder): os.makedirs(folder);
 
-    rtpath=f"{folder}\\{ob.name}_";
+    rtpath=f"{folder}\\{ob.lytools.par.name}_";
 
     rend.bake.use_selected_to_active=0;
     rend.engine='CYCLES';
 
-    rend.bake.use_pass_direct=0;
-    rend.bake.use_pass_indirect=0;
-    rend.bake.use_pass_color=1;
+    rend.bake.margin=int(ob.lytools.res/32);
 
 #   ---     ---     ---     ---     ---
-# loop thru mixes
+# loop thru mixes A
 
+    print(f"Baking mix: ", end='', flush=1);
     for i in range(len(ob.data.materials)):
 
-        print(f"Baking mix {i}: ", end='', flush=1);
-        ob.lytools.mix_idex=i;
+        mat=ob.data.materials[i]; ntree=mat.node_tree;
+        cbake=ntree.nodes[f"CBAKE{i}"]; ntree.nodes.active=cbake;
+        abake=ntree.nodes[f"ABAKE{i}"];
 
-        mat=ob.active_material; ntree=mat.node_tree;
+        cbake.image.generated_width=ob.lytools.res;
+        cbake.image.generated_height=ob.lytools.res;
+        abake.image.generated_width=ob.lytools.res;
+        abake.image.generated_height=ob.lytools.res;
 
-        cout = ntree.nodes["OUTCOLOR"];
+        mout=ntree.nodes["MATOUT"];
+        cout=ntree.nodes["OUTCOLOR"];
+        ntree.links.new(cout.outputs[0], mout.inputs[0]);
 
-        nmix, cmix = ntree.nodes["MIXNORMAL"], ntree.nodes["MIXCOLOR"];
-        omix, nbaketo = ntree.nodes["MIXORM"], ntree.nodes["NBAKE"];
-        fmix, amix = ntree.nodes["FRESNELMIX"], ntree.nodes["MIXALPHA"];
-        aobaketo, ncombined = ntree.nodes["AOBAKE"], ntree.nodes["NCOMB"];
-        result, resalpha = ntree.nodes["RESULT"], ntree.nodes["RESALPHA"];
-
-        ntree.nodes.active=result;
-        result.image.generated_width=ob.lytools.res;
-        result.image.generated_height=ob.lytools.res;
-        resalpha.image.generated_width=ob.lytools.res;
-        resalpha.image.generated_height=ob.lytools.res;
+        cmix=ntree.nodes["MIXCOLOR"];
+        amix=ntree.nodes["MIXALPHA"];
 
 #   ---     ---     ---     ---     ---
-# bake albedo
+# bake first three maps
 
-        print("albedo, ", end='', flush=1); impath=rtpath+f"albedo{i}.png";
+    for key in ["albedo", "orm", "normal"]:
 
-        SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
-        result.image.save_render(impath);
+        print(f"{key}, ", end='', flush=1);
 
-        ntree.nodes.active=resalpha;
-        ntree.links.new(amix.outputs[0], cout.inputs[0]);
-        SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
-
-        result.image.source='FILE'; result.image.filepath=impath;
-        STALPHA(result.image, resalpha.image); result.image.save_render(impath);
-
-        result.image.source='GENERATED';
-
-        ntree.nodes.active=result;
-        ntree.links.new(cmix.outputs[0], cout.inputs[0]);
-
-#   ---     ---     ---     ---     ---
-# set orm+e and bake
-
-        print("ORM+E, ", end='', flush=1); impath=rtpath+f"orm{i}.png";
-
-        for h in range(4):
-
-            node=ntree.nodes[f"IM{h}"];
-            if node.image.name != "DUMMY":
-                base=node.image.name.split("_")[0];
-                node.image=bpy.data.images[base+"_orm"];
+        for i in range(len(ob.data.materials)):
+            mat=ob.data.materials[i]; ntree=mat.node_tree;
+            for h in range(4):
+                node=ntree.nodes[f"IM{h}"];
+                if node.image.name != "DUMMY":
+                    base=node.image.name.split("_")[0];
+                    node.image=bpy.data.images[f"{base}_{key}"];
 
         SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
-        result.image.save_render(impath);
 
-        ntree.nodes.active=resalpha;
-        ntree.links.new(amix.outputs[0], cout.inputs[0]);
-        SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
+        for i in range(len(ob.data.materials)):
+            mat=ob.data.materials[i]; ntree=mat.node_tree;
+            cbake=ntree.nodes[f"CBAKE{i}"];
+            abake=ntree.nodes[f"ABAKE{i}"]; ntree.nodes.active=abake;
 
-        result.image.source='FILE'; result.image.filepath=impath;
-        STALPHA(result.image, resalpha.image); result.image.save_render(impath);
+            impath=rtpath+f"{key}{i}.png";
+            cbake.image.save_render(impath);
 
-        result.image.source='GENERATED';
-
-        ntree.nodes.active=result;
-        ntree.links.new(cmix.outputs[0], cout.inputs[0]);
-
-#   ---     ---     ---     ---     ---
-# set normal and bake
-
-        print("normals, ", end='', flush=1); impath=rtpath+f"normal{i}.png";
-
-        for h in range(4):
-
-            node=ntree.nodes[f"IM{h}"];
-            if node.image.name != "DUMMY":
-                base=node.image.name.split("_")[0];
-                node.image=bpy.data.images[base+"_normal"];
+            amix=ntree.nodes["MIXALPHA"];
+            cout=ntree.nodes["OUTCOLOR"];
+            ntree.links.new(amix.outputs[0], cout.inputs[0]);
 
         SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
-        result.image.save_render(impath);
 
-        ntree.nodes.active=resalpha;
-        ntree.links.new(amix.outputs[0], cout.inputs[0]);
-        SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
+        for i in range(len(ob.data.materials)):
 
-        result.image.source='FILE'; result.image.filepath=impath;
-        STALPHA(result.image, resalpha.image); result.image.save_render(impath);
+            mat=ob.data.materials[i]; ntree=mat.node_tree;
+            cbake=ntree.nodes[f"CBAKE{i}"];
+            abake=ntree.nodes[f"ABAKE{i}"];
 
-        result.image.source='GENERATED';
+            impath=rtpath+f"{key}{i}.png";
+            cbake.image.source='FILE';
+            cbake.image.filepath=impath;
 
-        ntree.nodes.active=result;
-        ntree.links.new(cmix.outputs[0], cout.inputs[0]);
+            STALPHA(cbake.image, abake.image);
+            cbake.image.save_render(impath);
+
+            cbake.image.source='GENERATED';
+            ntree.nodes.active=cbake;
+
+            cmix=ntree.nodes["MIXCOLOR"];
+            cout=ntree.nodes["OUTCOLOR"];
+            ntree.links.new(cmix.outputs[0], cout.inputs[0]);
 
 #   ---     ---     ---     ---     ---
 # set fresnel out and bake
 
-        print("fresnel... ");
+    print("fresnel... ");
+
+    for i in range(len(ob.data.materials)):
+
+        mat=ob.data.materials[i]; ntree=mat.node_tree;
+        fmix=ntree.nodes["FRESNELMIX"];
 
         for h in range(4):
 
@@ -281,8 +263,16 @@ def BKMIXMAT():
 
         ntree.links.new(fmix.outputs[0], ntree.nodes["MATOUT"].inputs[0]);
 
-        SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
-        result.image.save_render(rtpath+f"fresnel{i}.png");
+    SHUT_OPS(bpy.ops.object.bake, [], {'type':'EMIT'});
+
+    for i in range(len(ob.data.materials)):
+        mat=ob.data.materials[i]; ntree=mat.node_tree;
+
+        cbake=ntree.nodes[f"CBAKE{i}"];
+        cbake.image.save_render(rtpath+f"fresnel{i}.png");
+
+        cmix=ntree.nodes["MIXCOLOR"];
+        cout=ntree.nodes["OUTCOLOR"];
         ntree.links.new(cmix.outputs[0], cout.inputs[0]);
 
 #   ---     ---     ---     ---     ---
@@ -299,6 +289,41 @@ def BKMIXMAT():
         ntree.links.new(ntree.nodes["OUTCOLOR"].outputs[0], ntree.nodes["MATOUT"].inputs[0]);
 
     ob.lytools.mix_idex=0;
+
+#   ---     ---     ---     ---     ---
+# create copy for unify mix
+
+    cl=bpy.context.scene.objects.link(ob.copy()).object;
+    cl.data=cl.data.copy();
+
+    ob.name, cl.name = cl.name, ob.name;
+    ob.data, cl.data = cl.data, ob.data;
+
+    for key, _key in zip(ob.lytools.__dict__.keys(), cl.lytools.__dict__.keys()):
+        setattr(cl.lytools, key, cl.lytools[key]);
+        setattr(ob.lytools, _key, ob.lytools[_key]);
+
+    ob.name=f"{cl.lytools.par.name}_UNI"; ob.data.name=ob.name;
+
+    for _ in range(len(cl.data.materials)-1):
+        ob.active_material_index=0;
+        bpy.ops.object.material_slot_remove();
+
+    cmat=bpy.data.materials["COLORMASKING"].copy();
+    cmat.name=f"{ob.lytools.par.name}_KOLORMASK_UNIFY";
+    ob.data.materials[0]=cmat; nodes=cmat.node_tree.nodes;
+
+    nodes["Attribute"].attribute_name="Mix";
+
+    for i in range(len(cl.data.materials)):
+        imnode=nodes[f"IM{i}"]; imname=f"{ob.lytools.par.name}{i}";
+        if imname not in bpy.data.images:
+            im=bpy.data.images.new(imname, height=ob.lytools.res, width=ob.lytools.res);
+            im.source='FILE'; im.filepath=f"{rtpath}albedo{i}.png";
+
+        imnode.image=im;
+
+    cl.select=0;
 
 #   ---     ---     ---     ---     ---
 
@@ -506,13 +531,6 @@ class LYT_MixObjSettings(PropertyGroup):
 
     );
 
-    mix_name=StringProperty(
-
-        name        = "Parent name",
-        description = "Name of object the mix is derived from",
-
-    );
-
     f0=EnumProperty(
 
         items       = GTF0,
@@ -531,6 +549,9 @@ class LYT_MixObjSettings(PropertyGroup):
         max         = 8192
 
     );
+
+    par=PointerProperty(type=Object);
+    final=PointerProperty(type=Object);
 
 #   ---     ---     ---     ---     ---
 
