3f29d49f97acfbdcf26057b27f3f0d29d07f663b
Liebranca <dantepaola25@gmail.com>
2023-04-27 02:39:00 -0300

    crkwrite test

--- arcana/Bytes.py
+++ arcana/Bytes.py
@@ -14,6 +14,8 @@
 # ---   *   ---   *   ---
 # deps
 
+from mathutils import Vector;
+from Avt.cwrap import real,wide,ftb;
 from .Fmat import *;
 
 # ---   *   ---   *   ---
@@ -73,6 +75,18 @@ def unfrac_u8(b):
   return unfrac(b,STEP[7],BITS[7],True);
 
 # ---   *   ---   *   ---
+# TODO
+#
+#   move this somewhere else
+#   where mathstuffs reside
+#
+# calculate bitangent handedness
+
+def bhand (t,b,n):
+  t,b,n=Vector(t),Vector(b),Vector(n);
+  return -1 if (n.cross(t)).dot(b) < 0 else 1;
+
+# ---   *   ---   *   ---
 
 class CRK:
 
@@ -119,6 +133,12 @@ class CRK:
     ],
 
   );
+
+  # size of single raw vert
+  DUMP_STRIDE=48;
+
+  # verts + indices + poses + matidex
+  DUMP_HED_SZ=2+2+2+2;
 
 # ---   *   ---   *   ---
 
@@ -213,5 +233,161 @@ class CRK:
         out.append([verts,indices]);
 
     return CRK.bl_topy(out);
+
+# ---   *   ---   *   ---
+# write blender mesh data to
+# binary float dump
+#
+# this is later read at C-side
+# for further processing
+
+  @staticmethod
+  def bl_to_fdump(ob,fpath):
+
+    me    = ob.data;
+    mat   = 0;
+    poses = 1;
+
+    # serialize metadata
+    with open(f"{fpath}_meta",'wb+') as f:
+      f.write(CRK.bl_write_meta(me,poses,mat));
+
+    # ^serialize each pose
+    for i in range(0,poses):
+
+      # TODO: bl_adv_pose for animated meshes
+      with open(f"{fpath}_pose{i}",'wb+') as f:
+        f.write(CRK.bl_write_pose(ob));
+
+# ---   *   ---   *   ---
+# ^takes note of metadata
+
+  @staticmethod
+  def bl_write_meta(me,poses,mat):
+
+    hbuff=bytearray(CRK.DUMP_HED_SZ);
+
+    hbuff[0:2]=ftb(wide,[len(me.vertices)]);
+    hbuff[2:4]=ftb(wide,[len(me.polygons)]);
+    hbuff[4:6]=ftb(wide,[poses]);
+    hbuff[6:8]=ftb(wide,[mat]);
+
+    return hbuff;
+
+# ---   *   ---   *   ---
+# ^called once per frame or static
+
+  @staticmethod
+  def bl_write_pose(ob):
+
+    me    = ob.data;
+
+    ibuff = bytearray(len(me.polygons) * 6);
+    vbuff = bytearray(
+      len(me.vertices) * CRK.DUMP_STRIDE
+
+    );
+
+    # get bounding box size
+    bbuff=CRK.bl_write_box(ob);
+
+    # serialize pose
+    CRK.bl_write_cords(me,vbuff,ibuff);
+    CRK.bl_write_tangents(me,vbuff);
+
+    return bbuff + ibuff + vbuff;
+
+# ---   *   ---   *   ---
+# ^serialize bounding box
+
+  @staticmethod
+  def bl_write_box(ob):
+
+    bbuff=bytearray(3*4);
+    bbuff[ 0 :  4]=ftb(real,[ob.dimensions.x/2]);
+    bbuff[ 4 :  8]=ftb(real,[ob.dimensions.z]);
+    bbuff[ 8 : 12]=ftb(real,[ob.dimensions.y/2]);
+
+    return bbuff;
+
+# ---   *   ---   *   ---
+# ^write vertex cords, normals, uvs
+# and indices to pre-allocated buffers
+
+  @staticmethod
+  def bl_write_cords(me,vbuff,ibuff):
+
+    loops = me.uv_layers.active.data;
+    idex  = 0;
+
+    for face in me.polygons:
+      for vi,li in zip(
+        face.vertices,
+        face.loop_indices
+
+      ):
+
+        svi  = vi * CRK.DUMP_STRIDE;
+        loop = me.loops[li];
+        vert = me.vertices[vi];
+
+        # write coords
+        co=vert.co;
+        vbuff[svi +  0 : svi +  4]=ftb(real,[ co[0]]);
+        vbuff[svi +  4 : svi +  8]=ftb(real,[ co[2]]);
+        vbuff[svi +  8 : svi + 12]=ftb(real,[-co[1]]);
+
+        # write normals
+        n=vert.normal;
+        vbuff[svi + 12 : svi + 16]=ftb(real,[ n[0]]);
+        vbuff[svi + 16 : svi + 20]=ftb(real,[ n[2]]);
+        vbuff[svi + 20 : svi + 24]=ftb(real,[-n[1]]);
+
+        # write uvs
+        uv=loops[li].uv;
+        vbuff[svi + 40 : svi + 44]=ftb(real,[uv[0]]);
+        vbuff[svi + 44 : svi + 48]=ftb(real,[uv[1]]);
+
+        # write indices
+        ibuff[idex+0:idex+2]=vi.to_bytes(2,'little');
+        idex+=2;
+
+# ---   *   ---   *   ---
+# ^write tangents and bitangents
+
+  @staticmethod
+  def bl_write_tangents(me,vbuff):
+
+    me.calc_tangents();
+
+    for face in me.polygons:
+      for vi,li in zip(
+        face.vertices,
+        face.loop_indices
+
+      ):
+
+        svi  = vi * CRK.DUMP_STRIDE;
+        loop = me.loops[li];
+        vert = me.vertices[vi];
+
+        # write tangent
+        t=loop.tangent;
+        t=[t[0],t[2],-t[1]];
+        vbuff[svi + 24 : svi + 28]=ftb(real,[t[0]]);
+        vbuff[svi + 28 : svi + 32]=ftb(real,[t[1]]);
+        vbuff[svi + 32 : svi + 36]=ftb(real,[t[2]]);
+
+        # ^bitangent is skipped
+        # we calculate it's handedness instead
+        b=loop.bitangent;
+        n=vert.normal;
+        b=[b[0],b[2],-b[1]];
+        n=[n[0],n[2],-n[1]];
+
+        h=bhand(t,b,n);
+        vbuff[svi + 36 : svi + 40]=ftb(real,[h]);
+
+    me.free_tangents();
 
 # ---   *   ---   *   ---
